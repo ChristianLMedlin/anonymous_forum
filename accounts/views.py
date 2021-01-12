15e5fedc472e8a70a30af7465574566169b2c86a
Christian Medlin <ChristianLMedlin@gmail.com>
2021-01-11 19:59:55 -0500

    password is no longer given as a response when requesting accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .serializers import UserSerializer
 from .models import User
@@ -7,3 +8,7 @@ from .models import User
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def list(self, request):
+        serializer_class = UserSerializer(self.queryset, many=True)
+        return Response(serializer_class.data)
